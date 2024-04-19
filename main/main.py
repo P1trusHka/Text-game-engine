6c1c5774dae84c52f3b2f1ccc7b6dda4cb2d587a
Petrushenko <petrushenko@mirea.ru>
2024-04-19 22:41:17 +0300

    +main and other

---
+++ main/main.py
@@ -0,0 +1,11 @@
+
+def froom():
+    print("You woke up in a strange dark place. \nLooking around you saw an empty room. \nIt only has 3 doors. We need to get out. \n\nWhich door will you choose?")
+    switch{
+        case 1:
+    }
+def main():
+    print("\nYou are at the beginning of the maze.\nYour life is in your hands!\n")
+    froom()
+
+main()
